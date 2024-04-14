b77516c3865307fc0e92e413eb40299fb6fc8a5d
zhaolj <zhaolj@gmail.com>
2024-04-14 20:34:33 +0800

    Initial commit

---
+++ src/main/java/com/gw/mod/demo/GWDemoModClient.java
@@ -0,0 +1,13 @@
+package com.gw.mod.demo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GWDemoModClient implements ClientModInitializer {
+    /**
+     *
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/gw/mod/demo/GWDemoModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.gw.mod.demo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GWDemoModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
